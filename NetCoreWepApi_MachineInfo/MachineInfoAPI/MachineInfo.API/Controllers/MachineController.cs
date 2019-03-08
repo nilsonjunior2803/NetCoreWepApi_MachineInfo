c6d5586fb65d1a6b8f0ffa651c2209d42965892d
Nilson <nilson.junior@farfetch.com>
2019-03-08 19:16:46 +0000

    First commit
    First commit of this project;

---
+++ NetCoreWepApi_MachineInfo/MachineInfoAPI/MachineInfo.API/Controllers/MachineController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MachineInfo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachineController : ControllerBase
+    {
+        // GET api/machine/Detail
+        [HttpGet, Route("Detail")]
+        public ActionResult<string> GetMAchineDetails()
+        {
+            var machineName = Environment.MachineName;
+            var hostAddress = Dns.GetHostAddresses(Environment.MachineName)[0].ToString();
+
+            HttpContext.Response.Headers.Add("MachineName", machineName);
+            HttpContext.Response.Headers.Add("hostAddress", hostAddress);
+
+            return $"Machine name: {machineName} - Host Address: {hostAddress} ";
+        }
+    }
+}
